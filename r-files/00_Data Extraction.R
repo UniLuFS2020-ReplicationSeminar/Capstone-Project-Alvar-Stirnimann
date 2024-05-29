56c37c54da17b1fa82d8108661f2df678da29b7e
Alvar Stirnimann <stirnimann.alvar@gmail.com>
2024-05-29 16:05:17 +0200

    Data preparation Tourism

--- r-files/00_Data Extraction.R
+++ r-files/00_Data Extraction.R
@@ -6,7 +6,7 @@ library(httr)
 library(jsonlite)
 library(readxl)
 
-
+## Download Toursim Data
 # URL of the XLS file of Tourism Activity in Switzerland
 url <- "https://dam-api.bfs.admin.ch/hub/api/dam/assets/29045003/master"
 
@@ -16,6 +16,7 @@ file_destination <- file.path(getwd(), "data_orig", "data_original_tourism.xlsx"
 # Download the file and save to "data_orig" folder
 download.file(url, file_destination, mode="wb")
 
+## Download GDP Data
 # URL of the XLS file of GDP in Switzerland
 url_gdp <- "https://dam-api.bfs.admin.ch/hub/api/dam/assets/28405424/master"
 
@@ -23,4 +24,16 @@ url_gdp <- "https://dam-api.bfs.admin.ch/hub/api/dam/assets/28405424/master"
 file_destination_gdp <- file.path(getwd(), "data_orig", "data_original_gdp.xlsx")
 
 # Download the file and save to "data_orig" folder
-download.file(url, file_destination_gdp, mode="wb")
+download.file(url_gdp, file_destination_gdp, mode="wb")
+
+## Download detailed Tourism Data
+# URL of the Excel file
+url_tourism_2022 <- "https://dam-api.bfs.admin.ch/hub/api/dam/assets/30405413/appendix"
+
+# Destination file path on your local machine
+file_destination_tourism_2022 <- file.path(getwd(), "data_orig", "data_original_tourism_2022.xlsx")
+
+# Download the file and save to "data_orig" folder
+download.file(url_tourism_2022, file_destination_tourism_2022, mode="wb")
+
+## Download Population Data
--- r-files/01_Data Preparation.R
+++ r-files/01_Data Preparation.R
@@ -1,8 +1,52 @@
-# Data Preparation
+### Data Preparation
 
-# List all sheets in the Excel file
-sheets <- excel_sheets(file_destination)
-print(sheets)
+# Load libraries
+library(readxl)
+library(dplyr)
+library(readr)
+install.packages("dplyr")
 
-# Read a specific sheet by name or by index
-data_sheet1 <- read_excel(file_destination, sheet = sheets[1])  # Replace 1 with the index of the sheet you want
+# Load the data
+gdp_data <- read_excel("data_orig/data_original_gdp.xlsx")
+tourism_data <- read_excel("data_orig/data_original_tourism_2022.xlsx", sheet = "T2.2.6")
+
+# Check the structure of the data
+head(gdp_data)
+head(tourism_data)
+
+# Check the column names to ensure you're referencing them correctly in the next steps
+colnames(gdp_data)
+colnames(tourism_data)
+
+## Data Preparation Tourism
+selected_data_tourism <- tourism_data[-c(1,2,3), c(1, 4, 7, 10, 13)]
+head(selected_data_tourism)
+colnames(selected_data_tourism)
+
+# Drop columns with all NA values
+selected_data_tourism <- selected_data_tourism
+
+# Rename columns
+data_renamed_tourism <- selected_data_tourism %>%
+  rename(
+    "Region" = 1,
+    "2019" = 2,
+    "2020" = 3,
+    "2021" = 4,
+    "2022" = 5
+  )
+
+# Check
+head(data_renamed_tourism, n = 20)
+
+# Drop Columns after Aargau and Solothurn Region
+data_tourism_final <- data_renamed_tourism[-c(15:26),]
+head(data_tourism_final, n = 20)
+
+# Save to CSV
+write.csv(data_tourism_final, file.path(getwd(), "data_prep", "data_prep_tourism_2022.csv"))
+
+
+## Data Preparation GDP
+
+          

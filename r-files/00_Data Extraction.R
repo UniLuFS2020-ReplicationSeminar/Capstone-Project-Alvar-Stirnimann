e9dcac20a611c4319df484ebd348df049de8afc5
Alvar Stirnimann <stirnimann.alvar@gmail.com>
2024-05-31 21:17:56 +0200

    Tidy up the code

--- r-files/00_Data Extraction.R
+++ r-files/00_Data Extraction.R
@@ -5,16 +5,6 @@ library(httr)
 library(jsonlite)
 library(readxl)
 
-## Download Toursim Data General
-# URL of the XLS file of Tourism Activity in Switzerland 2022
-url <- "https://dam-api.bfs.admin.ch/hub/api/dam/assets/29045003/master"
-
-# Destination file path on your local machine
-file_destination <- file.path(getwd(), "data_orig", "data_original_tourism.xlsx")
-
-# Download the file and save to "data_orig" folder
-download.file(url, file_destination, mode="wb")
-
 ## Download GDP Data
 # URL of the XLS file of GDP in Switzerland
 url_gdp <- "https://dam-api.bfs.admin.ch/hub/api/dam/assets/28405424/master"

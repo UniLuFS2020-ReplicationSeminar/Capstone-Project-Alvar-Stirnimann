131c548eafe24ccd268b7e7ecbc2088386c219f8
Alvar Stirnimann <stirnimann.alvar@gmail.com>
2024-05-31 16:18:37 +0200

    Analysis update

--- r-files/05_Analysis.R
+++ r-files/05_Analysis.R
@@ -7,8 +7,6 @@ library(ggrepel)
 library(lme4)
 library(tidyr)
 
-install.packages("lattice")
-
 # Load the combined data
 combined_data <- read_csv("data_prep/data_prep_combined_final.csv", col_types = cols(
   .default = col_double(),
@@ -39,16 +37,18 @@ head(combined_data, n=20)
 #combined_data[tourism_columns] <- lapply(combined_data[tourism_columns], as.numeric)
 #combined_data[gdp_columns] <- lapply(combined_data[gdp_columns], as.numeric)
 
-# Reshapte the data from wide to long format
+# Reshape the data from wide to long format
 combined_data_long <- combined_data %>%
   pivot_longer(cols = -c(Region), 
                names_to = c("Year", ".value"), 
                names_pattern = "(\\d{4})_(.*)") %>%
-  mutate(Year = as.numeric(Year))
+  mutate(Year = as.numeric(Year),
+         Region = as.factor(Region))
 
 # Check
 head(combined_data_long, n=20)
 colnames(combined_data_long)
+str(combined_data_long)
 
 # Standarize the variables
 combined_data_long <- combined_data_long %>%
@@ -69,7 +69,7 @@ for(year in 2014:2021) {
 
 print(correlations)
 
-# Model
+## Model
 # Fit the linear mixed-effects model
 model <- lmer(GDP ~ Tourism_Nights + workers + (1 | Region) + (1 | Year), data = combined_data_long)
 
@@ -88,11 +88,38 @@ ggplot(combined_data_long, aes(x = Tourism_Nights, y = partial_residuals)) +
   geom_smooth(method = "lm", se = FALSE, color = "blue") +
   labs(title = "Effect of Tourism Nights on GDP (Controlled for Number of Workers)",
        x = "Standardized Tourism Nights",
-       y = "Partial Residuals of GDP") +
+       y = "Partial Residuals of GDP", 
+      color = "Region") +
   theme_classic()
 
 # Save plot to output folder
 ggsave("output/Effect of Tourism Nights on GDP (control for workers).png")
+
+# Calculate partial residuals for Tourism_Nights
+partial_residuals <- resid(model) + model.matrix(model)[, "Tourism_Nights"] * fixef(model)["Tourism_Nights"]
+
+# Add partial residuals to the data frame
+combined_data_long$partial_residuals <- partial_residuals
+
+# Plot the partial residuals showing the relationship between Tourism Nights and GDP
+ggplot(combined_data_long, aes(x = Tourism_Nights, y = partial_residuals)) +
+  geom_point(alpha = 0.6) +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  facet_wrap(~ Region, scales = "free_y") +
+  labs(title = "Effect of Tourism Nights on GDP (Controlled for Number of Workers)",
+       x = "Standardized Tourism Nights",
+       y = "Partial Residuals of GDP") +
+  theme_classic() +
+  theme(
+    panel.background = element_rect(fill = "white"),
+    plot.background = element_rect(fill = "white"),
+    legend.background = element_rect(fill = "white"),
+    legend.key = element_rect(fill = "white")
+  )
+
+# Save plot to output folder
+ggsave("output/Effect of Tourism Nights on GDP per Region (control for workers).png")
+
 
 
 # Plot scatter plot for 2021 with labeled points using ggplot2 and "combined_data" dataset
@@ -103,7 +130,6 @@ ggplot(combined_data, aes(x = `2021_Tourism_Nights`, y = `2021_GDP`, label = Reg
 
 # Save scatter plot to output folder
 ggsave("output/scatter_plot_2021_gdp__vs_tourism.png")
-
 
 
 
@@ -136,35 +162,4 @@ dotplot(ranef_model$Year, main = "Random Effects for Years")
 
 
 
-
-
-# Plot correlations
-ggplot(correlations, aes(x = Year, y = Correlation)) +
-  geom_line() +
-  geom_point() +
-  labs(title = "Yearly Correlation between Tourism Nights and GDP", x = "Year", y = "Correlation Coefficient")
-
-# Plot scatter plot for 2021 with labeled points using ggplot2
-ggplot(combined_data, aes(x = `2021_Tourism_Nights`, y = `2021_GDP`, label = Region)) +
-  geom_point() +
-  geom_text_repel() +
-  labs(title = "Scatter Plot of Tourism Nights vs GDP in 2021", x = "Tourism Nights", y = "GDP")
-
-# Save scatter plot to output folder
-ggsave("output/scatter_plot_2021_gdp__vs_tourism.png")
-
-## Regression Analysis
-# Fit a linear model for each year and gather coefficients
-results <- data.frame(Year = integer(), Intercept = numeric(), Slope = numeric(), R_squared = numeric())
-
-for(year in 2014:2021) {
-  lm_fit <- lm(formula = paste0("`", year, "_GDP` ~ `", year, "_Tourism_Nights`"), data = combined_data)
-  intercept <- coef(lm_fit)[1]
-  slope <- coef(lm_fit)[2]
-  r_squared <- summary(lm_fit)$r.squared
-  results <- rbind(results, data.frame(Year = year, Intercept = intercept, Slope = slope, R_squared = r_squared))
-}
-
-# Display regression results
-print(results)
 
